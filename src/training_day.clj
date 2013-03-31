ad601904fa91966763b5ce59ffba3774a3fc7619
Kalle Ylä-Anttila <kallenposti@gmail.com>
2013-03-31 23:32:56 +0300

    Lisää ensimmäiset tehtävät

--- src/training_day.clj
+++ src/training_day.clj
@@ -1,11 +1,9 @@
 (ns training-day)
 
-(def answer ":(")
+(def answer (num 42))
 
 (def hai "O HAI!")
 
-(defn square [x]
-  ":(")
+(defn square [x] (* x x))
 
-(defn average [a b]
-  ":(")
+(defn average [x y] (/ (+ x y) 2))
